1def29847e3def1974d92d80d4756eed04d55b81
DhruvPansuriya <152018368+DhruvPansuriya@users.noreply.github.com>
2025-02-03 22:27:28 +0530

    Update Landing.tsx

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from "../components/Navbar"
 import  BackgroundLinesDemo  from '../components/BackgroundLinesDemo';
 import HoverBorderGradientDemo from '../components/HoverBorderGradientDemo';
-
+import { ThreeDCardDemo } from '../components/ThreeDCardDemo';
 
 export function Landing() {
   return (
     <div className='relative'>
       {/* <Navbar/> */}
       <BackgroundLinesDemo/>
+      
       {/* <HoverBorderGradientDemo/> */}
     </div>
   
